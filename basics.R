206440e17dd86d567a099ebcf7d6b77525b7eb33
psmgeelen <psm.geelen@student.maastrichtuniversity.nl>
2018-02-01 09:13:06 +0100

    Basic Setup

---
+++ basics.R
@@ -0,0 +1,14 @@
+# Basic Operations script
+#
+# In this script the basic operations are described and explained. The basic syntax of
+# R is developed around "," (comma) seperations and "()" (bracket) grouping. In general 
+# the syntax (grammar of programming language) is similar to 
+#
+# object <- function(parameter1, vector2, dataframe3)
+#
+# In which is an object is the "thing" in which results can be stored, the function is 
+# something by which the information is processed (usually relates to a library), a
+# parameter which is this case is a value, a vector which is the same as a variable (a 
+# sequence of values) and a dataframe which is identical to a flat-table (just like a
+# sheet in excel is). Good to know is that R has simmplified datastructures that are all
+# compatable with each other. 
